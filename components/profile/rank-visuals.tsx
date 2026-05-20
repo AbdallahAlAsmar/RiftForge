798e1a20573fab6ef25e266c9c4efeeecc5bb797
AbdallahAlAsmar <killerpalestine@hotmail.com>
2026-05-20 12:34:11 +0300

    fixed griding

--- components/profile/rank-visuals.tsx
+++ components/profile/rank-visuals.tsx
@@ -79,10 +79,10 @@ export function RankAvatar({
           src={borderAsset}
           alt=""
           aria-hidden
-          className="pointer-events-none absolute inset-[-10%] z-20 h-[120%] w-[120%] object-contain"
+          className="pointer-events-none absolute inset-0 z-0 h-full w-full object-contain"
         />
       ) : null}
-      <div className="absolute inset-[18%] z-10 overflow-hidden rounded-full bg-secondary">
+      <div className="absolute inset-[30%] z-10 overflow-hidden rounded-full bg-secondary shadow-[0_0_0_2px_rgba(0,0,0,0.35)]">
         {src ? (
           // eslint-disable-next-line @next/next/no-img-element
           <img src={src} alt={alt} className={cn("h-full w-full object-cover", imageClassName)} />
